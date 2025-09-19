fc808797f6c24beb9cc9b9686fbedf451ed5f3c7
liblissz <liblissz3@gmail.com>
2025-09-19 22:32:44 +0100

    Update Feature.jsx

--- src/Componennts/Feature/Feature.jsx
+++ src/Componennts/Feature/Feature.jsx
@@ -57,7 +57,7 @@ const Features = () => {
           <p>
         We are a dynamic association dedicated to training innovative leaders for a brighter tomorrow. Our focus areas include promoting education, peace, and unity, youth employment, while providing humanitarian assistance to grassroots communities alongside environmental preservation.
      Our association was founded by Afuh Alfred Ngum, who rose above adversity after being abandoned by his father at the age of 3. Witnessing the struggles of vulnerable youths, lack of education opportunities, access to employment, skills, so, he decided that, he will create a better future for others for the sake of community development starting with grassroots efforts, he eventually registered ZOZAC Community in 2022, ZOZAC, a word from his created language which means "Togetherness Is Strength."
-
+<span></span>
           </p>
         </div>
         <div className="section-header">
@@ -84,4 +84,5 @@ const Features = () => {
 };
 
 export default Features;
+
 

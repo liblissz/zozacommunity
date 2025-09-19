28851e50c07d2d84b2a15624243967d657e4e43f
liblissz <liblissz3@gmail.com>
2025-09-20 00:01:51 +0100

    uu

--- src/Componennts/Samples/Samples.jsx
+++ src/Componennts/Samples/Samples.jsx
@@ -103,7 +103,9 @@ const Samples = () => {
   )}
 </p>
 
-<span className="btn-text" onClick={(=> window.location.href = `${item.price}`)}>Click to See details<i className="fas fa-arrow-right"></i></span>
+<span className="btn-text" onClick={()=> window.location.href = item.price}>
+  Click to See details<i className="fas fa-arrow-right"></i>
+  </span>
 
                   <Link to={`/picturepost/${item._id}`}>
                     <span className="btn-text">

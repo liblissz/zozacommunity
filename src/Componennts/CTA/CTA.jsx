d845c7552cb8384b91b717b732aa7bbab67d6271
liblissz <liblissz3@gmail.com>
2025-09-20 02:31:39 +0100

    fhfhfh

--- src/Componennts/CTA/CTA.jsx
+++ src/Componennts/CTA/CTA.jsx
@@ -10,18 +10,11 @@ const CTA = () => {
     <section className="cta">
       <div className="container">
         <div className="cta-content">
-          <h2>Volunteer With Us</h2>
-          <p>Your time, your voice, and your skills can help transform lives. Join our team of changemakers today!
-
-Partner With Us
-Whether you’re a local business, NGO, or international organization — let’s work together to build a stronger Africa.
-
-Donate
-Your support fuels our mission. Every donation helps us reach more people and expand our programs.</p>
+ 
           <div className="cta-buttons">
             <a href="tel: 674274276" target='_blank' className="btn-primary">Contact Us</a>
 
-            <a onClick={()=>setshow(!show)} className="btn-outline">DONATE</a>
+            <a onClick={()=> window.location.href="/vol" } className="btn-outline">Meet Our Vulonteers</a>
           </div>
         </div>
       </div>

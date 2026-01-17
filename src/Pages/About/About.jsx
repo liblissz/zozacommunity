60a38558c4f48148ef4b1e7ca3b172aa59484fa2
liblissz <liblissz3@gmail.com>
2026-01-17 10:52:38 +0100

    Update About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -96,6 +96,23 @@ ZOZAC Community was founded by Afuh Alfred Ngum, a visionary leader who overcame
             goal are essential for creating lasting impact and driving positive change.
           
           </li>
+
+                  <li>
+                  <b>Our Registration: </b>
+            <br/>
+                  <br/>
+ZOZAC Community is legally registered under the Cameroon government with the headquarters in Tiko sub-division, 
+                    Fako division, South West Region, Cameroon, West Africa. And it operates in conformity with the 
+                    prescription of Article 7 of Law No 90/054 of 19/12/1990 relating to the creation and functioning of 
+                    associations in Cameroon West Africa, with registration No; 1165/G.37/C84/VOLI/SAAJP. This registration 
+                    demonstrates our commitment to transparency, accountability, and good governance. We envisage a world where
+                    every individual has the opportunity to reach their full potential and contribute to a better society. We are
+                    dedicated to making a lasting impact and creating a brighter future for generations to come, through our work, 
+                    we aim to inspire positive change, promote education, peace building, social cohesion, community development, youth
+                    empowerment initiatives, and empower leaders to drive progress 
+                    and innovation for community development within self, Cameroon, Africa and beyond.
+          
+          </li>
       </ul>}
 
 
@@ -171,6 +188,7 @@ Vision: Be the leading force transforming, educating, and empowering individuals
 };
 
 export default About;
+
 
 
 

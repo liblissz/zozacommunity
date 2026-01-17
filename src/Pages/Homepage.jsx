1a77365f8ff20ad7d0b8652ca6fbe5768225ce47
liblissz <liblissz3@gmail.com>
2026-01-17 16:24:22 +0100

    Update Homepage.jsx

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,7 +5,7 @@ import UseCases from '../Componennts/Usecases/Usecases'
 import Samples from '../Componennts/Samples/Samples'
 import Pricing from '../Componennts/Prising/Prising'
 // import CTA from '../Componennts/CTA/CTA'
-import Email.jsx from "/Email.jsx"
+import ContactForm  from "/Email"
 
 const Homepage = () => {
   return (
@@ -22,4 +22,5 @@ const Homepage = () => {
 }
 
 export default Homepage
+
 
